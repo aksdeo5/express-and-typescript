acf2bc432e681e7c2836b9714d7ddb59fe25d8d4
aksdeo5 <akshaydeogaonkar2@gmail.com>
2024-02-06 15:35:49 +0530

    Finishing the REST Routes

--- routes/todos.ts
+++ routes/todos.ts
@@ -1,18 +1,38 @@
 import { Router } from "express";
 import { Todo } from "../models/todo";
 
-const todos: Todo[] = [];
+let todos: Todo[] = [];
 
 const router = Router();
 
 router.get("/api/todos", (req, res, next) => {
-	res.status(200).json({ todos });
+	res.status(200).json(todos);
 });
 
 router.post("/api/todos", (req, res, next) => {
-	const todo: Todo = { id: new Date().toISOString(), text: req.body.text };
+	const todo: Todo = {
+		id: new Date().toISOString().replace(/[.:]/g, "-"),
+		text: req.body.text,
+	};
 	todos.push(todo);
-	res.status(201).json({ todo });
+	res.status(201).json(todo);
+});
+
+router.delete("/api/todos/:id", (req, res, next) => {
+	const id = req.params.id;
+	const updatedTodos = todos.filter((todo) => todo.id !== id);
+	if (updatedTodos.length === todos.length) return res.status(404).json(null);
+	todos = updatedTodos;
+	res.status(204).json(null);
+});
+
+router.put("/api/todos", (req, res, next) => {
+	const updatedTodo = req.body;
+	const updatedTodos = todos.filter((todo) => todo.id !== updatedTodo.id);
+	if (updatedTodos.length === todos.length) return res.status(404).json(null);
+	todos = updatedTodos;
+	todos.push(updatedTodo);
+	res.status(200).json(updatedTodo);
 });
 
 export default router;
