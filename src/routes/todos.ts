72c07e1aedaed867531c26d858957c49afbc19b9
aksdeo5 <akshaydeogaonkar2@gmail.com>
2024-02-06 23:21:48 +0530

    Type Casting | Project Structure

--- src/routes/todos.ts
+++ src/routes/todos.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import { Todo } from "../models/todo";
 
+type TodosRequestBody = { text: string };
+
 let todos: Todo[] = [];
 
 const router = Router();
@@ -10,9 +12,10 @@ router.get("/api/todos", (req, res, next) => {
 });
 
 router.post("/api/todos", (req, res, next) => {
+	const body = req.body as TodosRequestBody;
 	const todo: Todo = {
 		id: new Date().toISOString().replace(/[.:]/g, "-"),
-		text: req.body.text,
+		text: body.text,
 	};
 	todos.push(todo);
 	res.status(201).json(todo);
@@ -26,13 +29,14 @@ router.delete("/api/todos/:id", (req, res, next) => {
 	res.status(204).json(null);
 });
 
-router.put("/api/todos", (req, res, next) => {
-	const updatedTodo = req.body;
-	const updatedTodos = todos.filter((todo) => todo.id !== updatedTodo.id);
-	if (updatedTodos.length === todos.length) return res.status(404).json(null);
-	todos = updatedTodos;
-	todos.push(updatedTodo);
-	res.status(200).json(updatedTodo);
+router.put("/api/todos/:id", (req, res, next) => {
+	const body = req.body as TodosRequestBody;
+	const id = req.params.id;
+	const targetTodoIndex = todos.findIndex((todo) => todo.id === id);
+	if (targetTodoIndex === -1) return res.status(404).json(null);
+	const targetTodo = todos[targetTodoIndex];
+	targetTodo.text = body.text;
+	res.status(200).json(targetTodo);
 });
 
 export default router;
